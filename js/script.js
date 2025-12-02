733ef8e4c4e6344f786db60ed5fb0023ee31d08e
mrovq <ryxaa2312@gmail.com>
2025-12-02 20:25:03 +0700

    upload pertama gwehj

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+
+// Klik di luar elemen
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
